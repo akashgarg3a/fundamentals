c4d6c0986c7861ce182bd1bb5729c5e44df70377
akash_garg <akashgarg3a@gmail.com>
2022-01-27 19:08:15 +0530

    Adding getter, setter and status

--- src/main/java/com/pluralsight/fundamentals/entity/Ticket.java
+++ src/main/java/com/pluralsight/fundamentals/entity/Ticket.java
@@ -22,8 +22,65 @@ public class Ticket {
     @JoinTable(name = "ticket_release", joinColumns = @JoinColumn(name="ticket_fk"), inverseJoinColumns = @JoinColumn(name="release_fk"))
     private Release release;
 
+    private String status;
+
+    public Ticket() {
+    }
+
+    public Ticket(String title, String description, Application application, Release release, String status) {
+        this.title = title;
+        this.description = description;
+        this.application = application;
+        this.release = release;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Application getApplication() {
+        return application;
+    }
+
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+
     public Release getRelease() {
         return release;
+    }
+
+    public void setRelease(Release release) {
+        this.release = release;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     @Override
